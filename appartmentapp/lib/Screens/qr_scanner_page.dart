7d4ad8c07f31e397a29184925baae534749d2a90
pspking2704 <phankieuphu@gmail.com>
2021-09-22 19:00:02 +0700

    update file

--- appartmentapp/lib/Screens/qr_scanner_page.dart
+++ appartmentapp/lib/Screens/qr_scanner_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
@@ -10,11 +12,25 @@ class _QrScannerState extends State<QrScanner>{
   final qrKey = GlobalKey(debugLabel: "QR");
 
   QRViewController? controller;
+  Barcode? barcode;
+
   @override 
   void dispose(){
     controller?.dispose();
     super.dispose();
   }
+
+  @override 
+  void reassemble() async{
+    super.reassemble();
+
+    if(Platform.isAndroid){
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+
   @override 
   Widget build(BuildContext context) => SafeArea(
     child: Scaffold(
@@ -22,8 +38,22 @@ class _QrScannerState extends State<QrScanner>{
         alignment: Alignment.center,
         children: <Widget>[
           buildQrView(context),
+          Positioned(bottom:10, child: buildResult(),),
+          Positioned(top:10, child: buildButtonController(),)
         ],
       ),
+    ),
+  );
+
+  Widget buildResult() => Container(
+    padding: EdgeInsets.all(12),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Colors.white24,
+    ),
+    child: Text(
+    barcode != null ? 'Result ${barcode!.code}' : 'Scan a code',
+    maxLines: 3,
     ),
   );
 
@@ -38,7 +68,62 @@ class _QrScannerState extends State<QrScanner>{
       cutOutSize: MediaQuery.of(context).size.width*0.8,
     ),
   );
+
+  Widget buildButtonController() => Container(
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    decoration: BoxDecoration(
+      color: Colors.white24,
+      borderRadius: BorderRadius.circular(8)
+    ),
+    child: Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: FutureBuilder<bool?>(
+            future: controller?.getFlashStatus(),
+            builder: (context, snapshot){
+              if(snapshot.data != null){
+                return Icon(
+                  snapshot.data! ? Icons.flash_on : Icons.flash_off
+                );
+              }
+              else{
+                return Container();
+              }
+            },
+          ),
+          onPressed: () async{
+            await controller?.toggleFlash();
+            setState(() {});
+          },
+        ),
+        IconButton(
+          onPressed: () async{
+            await controller?.flipCamera();
+            setState(() {}); 
+          },
+          icon: FutureBuilder(
+            future: controller?.getCameraInfo(),
+            builder: (context,snapshot){
+              if(snapshot.data != null){
+                return Icon(Icons.switch_camera);
+              }
+              else{
+                return Container();
+              }
+            }
+          ),
+        ),
+      ],
+    ),
+  );
+
   void onQRViewCreated(QRViewController controller){
     setState(() => this.controller = controller);
+
+    controller.scannedDataStream
+        .listen((barcode) => setState(() => this.barcode = barcode));
   }
 }
